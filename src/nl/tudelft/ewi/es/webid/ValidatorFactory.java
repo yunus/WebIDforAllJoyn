ab9b1bf87a353b06fb9c4ee8d5ce44b10fccbbbe
Yunus on Ultrabook <yunus@yanis.co>
2013-07-22 11:00:15 +0200

    ActAsDevice is the same as SameOwner

--- src/nl/tudelft/ewi/es/webid/ValidatorFactory.java
+++ src/nl/tudelft/ewi/es/webid/ValidatorFactory.java
@@ -19,7 +19,7 @@ public class ValidatorFactory {
 
 		try {
 			if (type.equals("ActAsHuman")){	return new ActAsHuman(Util.generateX509Cert(authCert), Util.generateX509Cert(suppCert));} 
-			else if (type.equals("ActAsDevice")){ return new ActAsDevice(Util.generateX509Cert(authCert), Util.generateX509Cert(suppCert));}
+			else if (type.equals("ActAsDevice") || type.equals("SameOwner") ){ return new ActAsDevice(Util.generateX509Cert(authCert), Util.generateX509Cert(suppCert));}
 			else if (type.equals("DirectTrust")){ return new DirectTrust(Util.generateX509Cert(authCert), Util.generateX509Cert(suppCert));}
 			else if (type.equals("InDirectTrust")){ return new InDirectTrust(Util.generateX509Cert(authCert), Util.generateX509Cert(suppCert));}
 		} catch (CertificateException e) {
